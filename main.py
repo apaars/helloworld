e4b1ef5063da052318c2192f14d68056dbab91a2
apaars <apaars@gmail.com>
2018-10-20 11:27:04 -0700

    Update main.py

--- main.py
+++ main.py
@@ -3,8 +3,13 @@ import math
 import time
 
 print(os.getcwd())
-print(math.isnan(5.5))
+print(math.isnan(5.50000))  # This change conflicts with change in the `ponies` branch.
 start = time.time()
 for _ in range(1000):
   _ = list(range(10000))
+print(time.time() - start)
+# Timing yet another loop.
+start = time.time()
+for _ in range(1000):
+  _ = list(range(1000))
 print(time.time() - start)
