fd87d77f5c177e57f88b4ad48c85ec597d2c284d
apaars <apaars@gmail.com>
2018-10-20 11:25:09 -0700

    adding pointless changes

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 import os
 import math
+import numpy as np
 import time
 
 print(os.getcwd())
-print(math.isnan(5.5))
+print(math.isnan(5.575875))
+print(math.isnan(np.nan))
 start = time.time()
 for _ in range(1000):
   _ = list(range(10000))
 print(time.time() - start)
+# Some pointless comment.
